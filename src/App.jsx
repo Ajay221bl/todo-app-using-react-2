f9951bd1b5fe68aed86eb417d4f7ee8d6772b299
Ajaydeep <ajaydeep221b@gmail.com>
2026-03-17 15:41:40 +0530

    counter App

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from "react";
 import "./App.css";
 
-// creating a counter button using react
-
-export default function App(){
 
 
-    const [count, setCount] = useState(0);
+function App() {
 
+
+    return (
+         <Counter></Counter>
     
+    )
+}
 
-    function Counter(){
-        return (
-            <div>
-            <h1>{count}</h1>
-            <button onClick={IncreaseCounter}>Counter</button>
-            </div>
-        )
-    }
 
+function Counter(){
+        const [count, setCount] = useState(0);
     function IncreaseCounter(){
         setCount(count + 1)
     }
 
-    
     return (
         <div>
-            <Counter></Counter>
+            <h1>{count}</h1>
+            <button onClick={IncreaseCounter}>Counter</button>
         </div>
     )
-}
-    
+}  
+
+
+
+
+
+export default App
+
+
