928253ea6639fddbc345653e7b5e6a7157eefd4a
Ajaydeep <ajaydeep221b@gmail.com>
2026-03-23 17:20:27 +0530

    created a basic component for the navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,48 @@ import { useState, useEffect} from "react";
 import "./App.css";
 import { PostComponent } from "./components";
 
-
+// Building a LinkedIn like Navigation Bar using react
 function App(){
-    // creating a state variable to store the state of the posts array 
-    let [posts, setPosts] = useState([]);
-    // for any change in the state variable the App() function gets re-rendered means the below
-    // map function gets executed again and again on the posts array, creating fresh new Posts using the Post Component
-    const postComponents = posts.map(post=> <PostComponent
-        profilePic={post.profilePic}
-        name={post.name}
-        designation={post.designation}
-        description={post.description}
-        contentImage={post.contentImage}
-         />)
-
-    // this function adds the new post to the posts array.
-    function addPost(){
-        setPosts([...posts, {
-           profilePic :"https://media.licdn.com/dms/image/v2/D5603AQE7vKPQpOICdg/profile-displayphoto-crop_800_800/B56ZvwwIiOIcAM-/0/1769270709332?e=1775692800&v=beta&t=TZAPblaOTU986S4JCXNpA-GjDibc9-w0wTEypCXw77c",
-           name : "Rana Mehtab",
-           designation:"Food Technologist | Quality Assurance Officer",
-           description:"These Five Steps ensure your inventory stays fresh and your operations stay lean",
-           contentImage:"https://media.licdn.com/dms/image/v2/D5610AQHhp9eYbE_00Q/image-shrink_1280/B56ZxlWp2oG4Ac-/0/1771226963634/HiringFreshers?e=1774796400&v=beta&t=NhjA92oMt0zP0j7b5HKY68hCRN9ojpt7DggqB0nTDFQ"
-   
-        }])
-    }
-    return ( 
-        <div>
-            <button onClick={addPost} style={{maxWidth: "100px"}}>Add Post</button>
-            <div>
-            {postComponents}
-            </div>
+    return (
+        <div style={{display:"flex", marginRight:"200px", marginLeft:"800px"}}>
+        <ButtonComponent 
+            logo={"https://cdn-icons-png.flaticon.com/128/15429/15429582.png"}
+            logoName={"Home"}
+            count={1}
+            />
+        <ButtonComponent
+            logo={"https://cdn-icons-png.flaticon.com/128/14233/14233355.png"}
+            logoName={"Jobs"}
+            />
+        <ButtonComponent
+            logo={"https://cdn-icons-png.flaticon.com/128/17561/17561655.png"}
+            logoName= {"My Network"}
+            />
+        <ButtonComponent
+            logo={"https://cdn-icons-png.flaticon.com/128/16689/16689811.png"}
+            logoName={"Messaging"}
+            />
+        <ButtonComponent
+            logo={"https://cdn-icons-png.flaticon.com/128/14283/14283678.png"}
+            logoName={"Notifications"}
+            />
         </div>
     )
-        
-        
+}
+
+
+function ButtonComponent({logo, logoName, count}){
+    return (
+        <div style={{textAlign:"center", marginRight: "20px"}}>
+            <div style={{display:"flex", alignItems:"start", justifyContent:"center"}}>
+                <img src={logo} style={{width: "30px" }}></img>
+                <div  style={{border:"solid black 0.5px", height:"10px", width:"10px"}}>{count}</div>
+            </div>
+            <div style={{fontSize:"15px"}}>{logoName}</div>
+        </div>
+    )
 }
 
 
 
 export default App
-
-
-
