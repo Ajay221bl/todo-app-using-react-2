8f1556d10449c9a8cfcfac8e3e9fac506e67c215
Ajaydeep <ajaydeep221b@gmail.com>
2026-03-23 15:40:38 +0530

    learned useState hook by building linkedIn post Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,47 @@
 import { useState, useEffect} from "react";
 import "./App.css";
-
+import { PostComponent } from "./components";
 
 
 function App(){
+    // creating a state variable to store the state of the posts array 
+    let [posts, setPosts] = useState([]);
+    // for any change in the state variable the App() function gets re-rendered means the below
+    // map function gets executed again and again on the posts array, creating fresh new Posts using the Post Component
+    const postComponents = posts.map(post=> <PostComponent
+        profilePic={post.profilePic}
+        name={post.name}
+        designation={post.designation}
+        description={post.description}
+        contentImage={post.contentImage}
+         />)
 
-    return (
+    // this function adds the new post to the posts array.
+    function addPost(){
+        setPosts([...posts, {
+           profilePic :"https://media.licdn.com/dms/image/v2/D5603AQE7vKPQpOICdg/profile-displayphoto-crop_800_800/B56ZvwwIiOIcAM-/0/1769270709332?e=1775692800&v=beta&t=TZAPblaOTU986S4JCXNpA-GjDibc9-w0wTEypCXw77c",
+           name : "Rana Mehtab",
+           designation:"Food Technologist | Quality Assurance Officer",
+           description:"These Five Steps ensure your inventory stays fresh and your operations stay lean",
+           contentImage:"https://media.licdn.com/dms/image/v2/D5610AQHhp9eYbE_00Q/image-shrink_1280/B56ZxlWp2oG4Ac-/0/1771226963634/HiringFreshers?e=1774796400&v=beta&t=NhjA92oMt0zP0j7b5HKY68hCRN9ojpt7DggqB0nTDFQ"
+   
+        }])
+    }
+    return ( 
         <div>
-        <Timer></Timer>
+            <button onClick={addPost} style={{maxWidth: "100px"}}>Add Post</button>
+            <div>
+            {postComponents}
+            </div>
         </div>
     )
+        
+        
 }
 
 
-function Timer(){
-    const [count, setCount] = useState(0);
-
-    //making sure that the setInterval function only gets rendered once by using useEffect() hook
-    useEffect(function(){
-        setInterval(function(){
-        setCount(function(count){
-            return count + 1 
-        })
-    }, 1000)
-    }, [])
-    
-
-    return (
-        <div>
-            <h1>{count}</h1>
-        </div>
-    )
-}
 
 export default App
+
+
+
