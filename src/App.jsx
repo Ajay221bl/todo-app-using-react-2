9120b4dffa203d645b2d8bbf7b97b941bf1543b2
Ajaydeep <ajaydeep221b@gmail.com>
2026-03-24 15:58:05 +0530

    created the tab buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,97 +2,23 @@ import { useState, useEffect} from "react";
 import "./App.css";
 import { PostComponent } from "./components";
 
-// Building a LinkedIn like Navigation Bar using react
-function App(){
-    const ButtonsArr = [
-        {
-            logo: "https://cdn-icons-png.flaticon.com/128/15429/15429582.png",
-            logoName: "Home"
-        },
-        {
-            logo:"https://cdn-icons-png.flaticon.com/128/14233/14233355.png",
-            logoName:"Jobs"
-        },
-        {
-            logo:"https://cdn-icons-png.flaticon.com/128/17561/17561655.png",
-            logoName:"My Network"
-        },
-        {
-            logo:"https://cdn-icons-png.flaticon.com/128/16689/16689811.png",
-            logoName:"Messaging"
-        },
-        {
-            logo: "https://cdn-icons-png.flaticon.com/128/14283/14283678.png",
-            logoName: "Notifications"
-        }
-    ]
-    const NavBarComponents = ButtonsArr.map(
-        Button=><NavBarButtonComponent 
-            logo={Button.logo}
-            logoName={Button.logoName}
-            count= {Button.count}
-            />)
-    
-    const CountButtonsArr = [
-        {
-            buttonName: "Home"
-        },
-        {
-            buttonName: "Jobs"
-        },
-        {
-            buttonName: "My Networks"
-        },
-        {
-            buttonName: "Messaging"
-        },
-        {
-            buttonName: "Notifications"
-        }
-    ]
-    const CountAddButtonComponents = CountButtonsArr.map(CountButton=>
+// fetching data from backend using react
 
-    <CountAddButton  buttonName={CountButton.buttonName}/>)
+function App(){
+    let [currentTab, setCurrentTab] = useState(1);
+    let [tabData, setTabData] = useState({});
+
+    
+    
     return (
         <div>
-            <div style={{display:"flex", marginRight:"200pk", marginLeft:"800px"}}>
-            {NavBarComponents}
-            </div>
-            <div>
-                {CountAddButtonComponents}
-            </div>
+            <button style={{color: currentTab==1 ? "red" : "black" }} onClick={()=>setCurrentTab(1)}>todo1</button>
+            <button style={{color: currentTab==2 ? "red": "black"}} onClick={()=>setCurrentTab(2)}>todo2</button>
+            <button style={{color: currentTab==3 ? "red": "black"}} onClick={()=>setCurrentTab(3)}>todo3</button>
+            <button style={{color: currentTab==4 ? "red" : "black"}} onClick={()=>setCurrentTab(4)}>todo4</button>
         </div>
     )
 }
-
-
-function NavBarButtonComponent({logo, logoName, count}){
-    return (
-        <div style={{textAlign:"center", marginRight: "20px"}}>
-            <div style={{display:"flex", alignItems:"start", justifyContent:"center"}}>
-                <img src={logo} style={{width: "30px" }}></img>
-                <div  style={{border:"solid black 0.5px", height:"10px", width:"10px", fontSize:"15px"}}>{count}</div>
-            </div>
-            <div style={{fontSize:"15px"}}>{logoName}</div>
-        </div>
-    )
-}
-
-function CountAddButton({buttonName, count}){
-    
-    function addCount(){
-        setCount(count + 1)
-    }
-    return (
-        <button onClick={addCount}>{buttonName}</button>
-    )
-}
-
 
 
 export default App
-
-
-
-
-
