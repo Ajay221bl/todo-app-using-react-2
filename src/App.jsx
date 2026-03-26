f1ccdb15e77964c0e49b6154cc673b0b1b18a4ed
Ajaydeep <ajaydeep221b@gmail.com>
2026-03-26 14:23:03 +0530

    created the unstoppable stopwatch

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,25 @@ import { useState, useEffect} from "react";
 import "./App.css";
 import { PostComponent } from "./components";
 
-// stopwatch in react
+// clock
 
 function App(){
-    let [showTimer, setShowTimer] = useState(true);
+    const [count, setCount] = useState(0);
 
-    useEffect(()=>{
-        setInterval(()=>{
-            setShowTimer(x=> !x)
-        }, 5000)
-
-        
-    }, [])
-
-    
-    return (
-        <div>{showTimer ? <Timer /> : <div></div> }</div>
-    )
-}
-
-function Timer(){
-    let [seconds, setSeconds] = useState(1);
-    
-
-    function increaseCount(){
-        setSeconds(prev=> prev + 1);
+    function startClock(){
+            setInterval(()=>{
+                setCount(prev=> prev + 1)
+            }, 1000)
     }
 
-    useEffect(()=>{
-        let clock = setInterval(()=>{
-            increaseCount()
-        }, 1000)
 
-        // cleanup logic here in return expression
-         return function(){
-            clearInterval(clock)
-         }
-    }, [])
-    return (
-        <div>{seconds} seconds elapsed</div>
+    return(
+        <div>
+            <div>{count}</div>
+            <button onClick={startClock}>Start</button>
+            <button>Stop</button>
+        </div>
+        
     )
 }
 
